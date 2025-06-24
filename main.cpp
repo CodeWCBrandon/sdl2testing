77cc582555b694d134941d7d2e0cac8d781ed76a
CodeWCBrandon <christian.brandon@binus.ac.id>
2025-06-24 13:38:18 +0700

    Initial setup

--- main.cpp
+++ main.cpp
@@ -2,31 +2,78 @@
 #include <iostream>
 #include <stdlib.h>
 
+void Update();
+void Start();
+bool Init();
+int Exit(int exitCode);
+
+
+SDL_Window* window;
+SDL_Surface* winSurface;
+
+// all technical setup
 int main() 
 {
+    if(!Init())
+    {
+        std::cout << "failed to intiliazing the SDL\n" << SDL_GetError() << std::endl;
+        return Exit(1);
+    }
+    
+    Start();
+
+    while(true)
+    {
+        Update();
+
+        //updating window
+        SDL_UpdateWindowSurface(window);    
+    }
+    
+    return Exit(0);
+}
+
+//Game logic
+void Start()
+{
+
+}
+
+//Updating every frames
+void Update()
+{
+    // Window background rectangle with colors
+    SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 128, 128,128));
+    
+}
+    
+bool Init()
+{
+    //Check is SDL Initialized without an error
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         std::cout << "failed to initializing the SDL\n" << SDL_GetError() << std::endl;
-        return 1;
+        return false;
     }
 
-    SDL_Window* window = SDL_CreateWindow("main", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    //Creating the window
+    window = SDL_CreateWindow("main", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 
+    //Checking the window 
     if(!window)
     {
         std::cout << "failed to show the window\n" << SDL_GetError() << std::endl; 
-        return 1;
+        return false;
     }
 
-    SDL_Surface* winSurface = SDL_GetWindowSurface(window);
+    //Creating the window surface
+    winSurface = SDL_GetWindowSurface(window);
+}
 
-    SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 128, 128,128));
-    
-    SDL_UpdateWindowSurface(window);    
-
-    SDL_Delay(5000);
-
+int Exit(int exitCode)
+{
+    //Quitting
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
+    return exitCode;
 }
